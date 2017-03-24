98f366b2cc4b4f1c7dd1e2010772dc497a8722bd
roma160498 <roma160498@yandex.ru>
2017-03-25 00:46:59 +0300

    first commit

---
+++ Office Post/EmailSender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace Office_Post
+{
+    class EmailSender
+    {
+        private string senderEmail;
+        private string name;
+        private string receiverEmail;
+        private string password;
+        private string domain = "";
+
+        public EmailSender(string senderEmail, string password, string name, string receiverEmail)
+        {
+            this.senderEmail = senderEmail;
+            this.name = name;
+            //this.receiverEmail = receiverEmail;
+            this.password = password;
+        }
+        public void SendEmail(string header, string message,List<string> members)
+        {
+            MailAddress Sender = new MailAddress(senderEmail, name);
+
+            int pos = senderEmail.IndexOf('@');
+            for (int i = pos + 1; i < senderEmail.Length; i++)
+                domain += senderEmail[i];
+            SmtpClient smtp = new SmtpClient("smtp." + domain, 587);
+            smtp.Credentials = new NetworkCredential(senderEmail, password);
+            smtp.EnableSsl = true;
+            for (int j = 0; j < members.Count; j++)
+            {
+                MailAddress Receiver = new MailAddress(members[j]);
+                MailMessage messageObject = new MailMessage(Sender, Receiver);
+
+                messageObject.Subject = header;
+                messageObject.Body = message;
+               
+            
+            smtp.Send(messageObject);
+
+            }
+        }
+    }
+}
---
+++ Office Post/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace Office_Post
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Byte[] info = new byte[1000];
+        List<string> members = new List<string>(); 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loadEmailsListButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myDialog = new OpenFileDialog();
+            myDialog.Filter = "Текстовый файл(*.txt)|*.txt";
+            myDialog.CheckFileExists = true;
+            myDialog.Multiselect = true;
+            if (myDialog.ShowDialog() == true)
+            {
+                using (StreamReader sr = new StreamReader(myDialog.FileName, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        members.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            string title;
+            string message;
+            title = titleBox.Text;
+            message = messageBox.Text;
+            EmailSender emailSender = new EmailSender(emailBox.Text, passwordBox.Text, nameBox.Text, receiverEmailBox.Text);
+            emailSender.SendEmail(title,message,members);
+        }
+    }
+}
